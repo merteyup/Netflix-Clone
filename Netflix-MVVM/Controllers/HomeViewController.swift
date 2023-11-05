afbb24f83878488616a7f14b6f17ac698db0dbec
merteyup <eupmrt@gmail.com>
2023-11-05 12:37:50 +0300

    Created 'HomeViewController'

---
+++ Netflix-MVVM/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
