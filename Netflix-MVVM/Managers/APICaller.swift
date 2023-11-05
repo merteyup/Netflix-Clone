b48b4b6cddd76fa35aca1c791b8b7faac4b361c0
merteyup <eupmrt@gmail.com>
2023-11-05 18:08:47 +0300

    Add API key for getting movie informations from TMDB.

---
+++ Netflix-MVVM/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let API_KEY = "183895713e73c8b81332f28e09768cc7"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+}
