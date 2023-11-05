54ea448b652277a864d524bfcf73c45a12d9b4bf
merteyup <eupmrt@gmail.com>
2023-11-05 20:45:20 +0300

    Add TitleCollectionViewCell to represent view elements.

---
+++ Netflix-MVVM/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+}
