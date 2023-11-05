8955d721378ab7efad86263aca9030642815091e
merteyup <eupmrt@gmail.com>
2023-11-05 17:23:26 +0300

    Add play button to left navbar item since Netflix logo made aspect ratio problems.

--- Netflix-MVVM/Controllers/HomeViewController.swift
+++ Netflix-MVVM/Controllers/HomeViewController.swift
@@ -37,9 +37,13 @@ class HomeViewController: UIViewController {
     }
     
     private func configureNavBar() {
-        var image = UIImage(named: "netflixLogo")
-        image = image?.withRenderingMode(.alwaysOriginal)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "play"), style: .done, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .label
     }
 }
 
