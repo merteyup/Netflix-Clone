45f4626e4ef27bf3c91a5c86e9f63ea62b8c411c
merteyup <eupmrt@gmail.com>
2023-11-05 21:50:34 +0300

    Add tableView to 'UpcomingViewController'

--- Netflix-MVVM/Controllers/UpcomingViewController.swift
+++ Netflix-MVVM/Controllers/UpcomingViewController.swift
@@ -8,11 +8,47 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
-
+    
+    private let upcomingTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.backgroundColor = .yellow
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        upcomingTable.dataSource = self
+        upcomingTable.delegate = self
+        
+        view.addSubview(upcomingTable)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+}
 
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Upcoming Table Cell"
+        return cell
+    }
+    
 }
