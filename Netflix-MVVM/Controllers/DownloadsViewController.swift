d0e4c69adc141e931eabae7470f3c2338250de91
merteyup <eupmrt@gmail.com>
2023-11-05 12:41:56 +0300

    Created 'DownloadsViewController'

---
+++ Netflix-MVVM/Controllers/DownloadsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadsViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+
+    }
+
+}
