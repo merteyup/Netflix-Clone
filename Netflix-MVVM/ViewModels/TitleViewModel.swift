26e98d2ed124e91822bbda8a16507bfee90faae9
merteyup <eupmrt@gmail.com>
2023-11-05 22:31:23 +0300

    Create TitleViewModel

---
+++ Netflix-MVVM/ViewModels/TitleViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TitleViewModel.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    
+    let titleName: String
+    let posterURL: String
+    
+}
