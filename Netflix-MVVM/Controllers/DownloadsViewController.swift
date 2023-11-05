cbe769b97bed08c71cc4e69bfb1aab99e48c0547
merteyup <eupmrt@gmail.com>
2023-11-05 12:50:36 +0300

    All view controllers background color changed to the system background.

--- Netflix-MVVM/Controllers/DownloadsViewController.swift
+++ Netflix-MVVM/Controllers/DownloadsViewController.swift
@@ -12,8 +12,7 @@ class DownloadsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
-
+        view.backgroundColor = .systemBackground
     }
 
 }
