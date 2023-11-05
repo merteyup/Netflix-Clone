6f15e7105d2209f908c38ed9e3402c8eabfb9572
merteyup <eupmrt@gmail.com>
2023-11-05 22:33:03 +0300

    Create TitleCollectionViewCell

---
+++ Netflix-MVVM/Views/TitleTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+   static let identifier = "TitleTableViewCell"
+    
+    private let titleButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        button.setImage(image, for: .normal)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: TitleTableViewCell.identifier)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(titleButton)
+        
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let titlePosterUIImageViewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterUIImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
+        let titleButtonConstraints = [
+            titleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            titleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(titlePosterUIImageViewConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(titleButtonConstraints)
+
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {return}
+        titlePosterUIImageView.sd_setImage(with: url)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
