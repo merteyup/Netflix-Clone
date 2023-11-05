50512af3c1e9a9a6f90e5a9cbe2a9889efe91571
merteyup <eupmrt@gmail.com>
2023-11-05 16:47:28 +0300

    Add Download Button to the 'HeroHeaderUIView'

--- Netflix-MVVM/Views/HeroHeaderUIView.swift
+++ Netflix-MVVM/Views/HeroHeaderUIView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let downloadButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor =  UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
     private let playButton : UIButton = {
         let button = UIButton()
         button.setTitle("Play", for: .normal)
@@ -31,6 +41,7 @@ class HeroHeaderUIView: UIView {
         addSubview(heroImageView)
         addGradient()
         addSubview(playButton)
+        addSubview(downloadButton)
         applyConstraints()
     }
     
@@ -40,7 +51,14 @@ class HeroHeaderUIView: UIView {
             playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
             playButton.widthAnchor.constraint(equalToConstant: 100)
         ]
+        
+        let downloadButtonConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -90),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
         NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
     }
     
     private func addGradient() {
