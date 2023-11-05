695a1f1a33696140aef01ef6305785594a75cff8
merteyup <eupmrt@gmail.com>
2023-11-05 12:41:08 +0300

    Created 'SearchViewController'

---
+++ Netflix-MVVM/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
