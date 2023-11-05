d9a6af631456099652483603bcde3f375aaaa066
merteyup <eupmrt@gmail.com>
2023-11-05 12:39:17 +0300

    Created 'UpcomingViewController'

---
+++ Netflix-MVVM/Controllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+}
