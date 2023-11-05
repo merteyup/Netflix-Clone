ecb5b87b667faacbb1293cb9a4e8b06b067a5ee4
merteyup <eupmrt@gmail.com>
2023-11-05 12:25:52 +0300

    Initial Commit

---
+++ Netflix-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 5.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
