b2b605cd630d5ac45a6782e79ef2c44c3ab52a6d
merteyup <eupmrt@gmail.com>
2023-11-06 14:59:17 +0300

    Make APICall from 'SearchViewController' use the result to update 'SearchResultsViewController'

--- Netflix-MVVM/Controllers/Core/SearchViewController.swift
+++ Netflix-MVVM/Controllers/Core/SearchViewController.swift
@@ -38,6 +38,8 @@ class SearchViewController: UIViewController {
         
         navigationController?.navigationBar.tintColor = .label
         fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -79,4 +81,31 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
         return 140
     }
         
+}
+
+
+extension SearchViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else { return }
+        
+        
+        APICaller.shared.search(with: query) { result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
 }
