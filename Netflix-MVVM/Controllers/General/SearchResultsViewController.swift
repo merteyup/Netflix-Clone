9852e3e0ad416d838fa1b8a4dffed4ea8b925fa7
merteyup <eupmrt@gmail.com>
2023-11-06 10:24:19 +0300

    Create properly designed searchResultsCollectionView in 'SearchResultsViewController'

--- Netflix-MVVM/Controllers/General/SearchResultsViewController.swift
+++ Netflix-MVVM/Controllers/General/SearchResultsViewController.swift
@@ -8,13 +8,46 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let searchResultsCollectionView : UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
+        return collectionView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
+        
+        searchResultsCollectionView.delegate = self
+        searchResultsCollectionView.dataSource = self
     }
     
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultsCollectionView.frame = view.bounds
+    }
  
+}
+
+extension SearchResultsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {return UICollectionViewCell()}
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
 }
