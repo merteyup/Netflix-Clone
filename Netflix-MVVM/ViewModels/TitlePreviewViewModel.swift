38a9e3ed4b63cd1a96dbb93b04a4b6da6a4da336
merteyup <eupmrt@gmail.com>
2023-11-06 18:11:03 +0300

    Add 'TitlePreviewViewModel'

---
+++ Netflix-MVVM/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 6.11.2023.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverView: String
+    
+}
