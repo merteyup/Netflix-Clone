a8a48443662527b54a375352d787e851a245fea3
merteyup <eupmrt@gmail.com>
2023-11-06 10:10:05 +0300

    Create 'SearchResultViewController' and re-arrange Controllers folder with Core and General structure.

---
+++ Netflix-MVVM/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix-MVVM
+//
+//  Created by Eyüp Mert on 6.11.2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+
+    }
+    
+
+ 
+}
