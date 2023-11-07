3609d470a3ce4c17b3b116e26da18f6af465e070
merteyup <eupmrt@gmail.com>
2023-11-07 22:00:04 +0300

    Create 'DownloadsViewController' with duplicate tableView

--- Netflix-MVVM/Controllers/Core/DownloadsViewController.swift
+++ Netflix-MVVM/Controllers/Core/DownloadsViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    private var titles: [TitleItem] = [TitleItem]()
+    
+    private let downloadedTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,4 +23,24 @@ class DownloadsViewController: UIViewController {
         view.backgroundColor = .systemBackground
     }
 
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell()}
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown",
+                                            posterURL: title.poster_path ?? ""))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
 }
