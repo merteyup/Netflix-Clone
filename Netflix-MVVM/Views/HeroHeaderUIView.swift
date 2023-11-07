745bfffdc67bc848a2eb6a518f1b57ca895af784
merteyup <eupmrt@gmail.com>
2023-11-07 16:06:58 +0300

    Show random image on 'HomeViewController' header  every time the app runs.

--- Netflix-MVVM/Views/HeroHeaderUIView.swift
+++ Netflix-MVVM/Views/HeroHeaderUIView.swift
@@ -36,7 +36,6 @@ class HeroHeaderUIView: UIView {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.image = UIImage(named: "shawshankImage")
         return imageView
     }()
     
@@ -73,6 +72,11 @@ class HeroHeaderUIView: UIView {
         ]
         gradientLayer.frame = bounds
         layer.addSublayer(gradientLayer)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {return}
+        heroImageView.sd_setImage(with: url)
     }
     
     override func layoutSubviews() {
