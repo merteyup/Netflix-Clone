c7e2426df4a28dfbd6f0b506b90764dc3c4eb81d
merteyup <eupmrt@gmail.com>
2023-11-07 09:45:32 +0300

    Configure layout in 'TitlePreviewViewController'.

--- Netflix-MVVM/Controllers/General/TitlePreviewViewController.swift
+++ Netflix-MVVM/Controllers/General/TitlePreviewViewController.swift
@@ -32,17 +32,23 @@ class TitlePreviewViewController: UIViewController {
     private let downloadButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .red
         button.setTitle("Download", for: .normal)
         button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
         return button
     }()
     
-    private let webView: WKWebView = WKWebView()
+    private let webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .systemBackground
         view.addSubview(webView)
         view.addSubview(titleLabel)
         view.addSubview(overviewLabel)
@@ -54,9 +60,10 @@ class TitlePreviewViewController: UIViewController {
     func configureConstraints() {
         
         let webViewConstraints = [
-            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.heightAnchor.constraint(equalToConstant: 250)
         ]
         
         let titleLabelConstraints = [
@@ -69,10 +76,20 @@ class TitlePreviewViewController: UIViewController {
             overviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
         ]
         
+        let downloadButtonConstraints = [
+            downloadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            downloadButton.topAnchor.constraint(equalTo: overviewLabel.bottomAnchor, constant: 25),
+            downloadButton.widthAnchor.constraint(equalToConstant: 140),
+            downloadButton.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
         NSLayoutConstraint.activate(webViewConstraints)
         NSLayoutConstraint.activate(titleLabelConstraints)
         NSLayoutConstraint.activate(overviewTitleLabelConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
 
     }
+    
+    
     
 }
